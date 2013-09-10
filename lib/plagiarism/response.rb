abeeb2072fb8e434e8d57126468bcfd01199721e
Zach Ohlgren <zach@ohlgren.me>
2013-09-10 16:08:35 -0700

    Add some convenience methods to response object

--- lib/plagiarism/response.rb
+++ lib/plagiarism/response.rb
@@ -12,13 +12,27 @@ module Plagiarism
     end
 
 
+    # Return true if the API call succeeded
     def success?
       @response.ok? && @doc.css("error").text == ""
     end
 
 
+    # Return true if the API call did not succeed
     def error?
       not success?
+    end
+
+
+    # Return the body of the response
+    def body
+      @response ? @response.response.body : nil
+    end
+
+
+    # Return the HTTP status code from the response
+    def status_code
+      @response ? @response.response.code : nil
     end
 
   end
